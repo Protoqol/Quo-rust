b89b41c5c93eea4c23ca6a26e47990ae2b939fc0
Quinten Schorsij <quinten@protoqol.nl>
2026-03-11 00:31:50 +0100

    (feat) transition to xxhash instead of sha256

--- src/info/hash.rs
+++ src/info/hash.rs
@@ -1,20 +1,16 @@
-#[cfg(feature = "hashing")]
-use sha2::{Digest, Sha256};
+use twox_hash::XxHash64;
 
 /// Get reproducible hash for grouping of same variables
 pub fn get_hash(var_type: &str, name: &str, package_name: &str) -> Option<String> {
-    #[cfg(feature = "hashing")]
-    {
-        let mut hasher = Sha256::new();
-        hasher.update(format!("{}:{}:{}", var_type, name, package_name));
-        Some(format!("{:x}", hasher.finalize()))
-    }
-
-    #[cfg(not(feature = "hashing"))]
-    {
-        let _ = (var_type, name, package_name);
-        None
-    }
+    let seed = 123456789;
+    // @TODO tests
+    Some(
+        XxHash64::oneshot(
+            seed,
+            &format!("{}:{}:{}", var_type, name, package_name).into_bytes(),
+        )
+        .to_string(),
+    )
 }
 
 #[cfg(test)]
@@ -23,17 +19,11 @@ mod tests {
 
     #[test]
     fn test_get_hash_none_without_feature() {
-        #[cfg(not(feature = "hashing"))]
         assert!(get_hash("i32", "var", "pkg").is_none()); // You never know for sure...
     }
 
     #[test]
-    #[cfg(feature = "hashing")]
     fn test_get_hash_consistency() {
         // Testing the hashing lib itself might be redundant, but I like having tests.
-        let hash1 = get_hash("i32", "var", "pkg");
-        let hash2 = get_hash("i32", "var", "pkg");
-        assert_eq!(hash1, hash2);
-        assert!(hash1.is_some());
     }
 }
